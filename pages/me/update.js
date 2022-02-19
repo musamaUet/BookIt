75e62e8cfcb5c69cf6088b20a99458edcc9b7745
M Usama Rabbani <musamarabbani@MP-Amir-sSE-61.local>
2022-02-20 00:55:30 +0500

    routes protection from frontend side

--- pages/me/update.js
+++ pages/me/update.js
@@ -1,26 +1,25 @@
-import Layout from '../components/layout/Layout';
-import Register from '../components/auth/Register';
+import React from 'react';
 import { getSession } from 'next-auth/react';
 
-export default function RegisterPage() {
+export default function UpdateProfile(props) {
 	return (
-		<Layout title='Login'>
-			<Register />
-		</Layout>
+		<div>
+			<h4>This is update profile page</h4>
+		</div>
 	);
 }
 
 export async function getServerSideProps(context) {
 	const session = await getSession({ req: context.req });
-	if (session) {
+	if (!session) {
 		return {
 			redirect: {
-				destination: '/',
+				destination: '/login',
 				permanent: false,
 			},
 		};
 	}
 	return {
-		props: {},
+		props: { session },
 	};
 }
