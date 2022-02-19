0735ebf586e22cdcb84a46aea9a54b57cf506fcd
M Usama Rabbani <musamarabbani@MP-Amir-sSE-61.local>
2022-02-20 00:27:36 +0500

    auth center finish with redux

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -34,11 +34,55 @@ const Header = () => {
 				</div>
 
 				<div className='col-3 mt-3 mt-md-0 text-center'>
-					<Link href='/login'>
-						<a className='btn btn-danger px-4 text-white login-header-btn float-right'>
-							Login
-						</a>
-					</Link>
+					{user ? (
+						<div className='ml-4 dropdown d-line'>
+							<a
+								className='btn dropdown-toggle mr-4'
+								id='dropDownMenuButton'
+								data-toggle='dropdown'
+								aria-haspopup='true'
+								aria-expanded='false'
+							>
+								<figure>
+									<img
+										width={'50px'}
+										height={'50px'}
+										src={user.avatar && user.avatar.url}
+										alt={user && user.name}
+										className='rounded-circle'
+									/>
+								</figure>
+								<span>{user && user.name}</span>
+							</a>
+							<div
+								className='dropdown-menu'
+								aria-labelledby='dropDownMenuButton'
+							>
+								<Link href='/bookings/me'>
+									<a className='dropdown-item'>My Bookings</a>
+								</Link>
+								<Link href='/me/update'>
+									<a className='dropdown-item'>Profile</a>
+								</Link>
+								<Link href='/'>
+									<a
+										className='dropdown-item text-danger'
+										onClick={logoutHandler}
+									>
+										Logout
+									</a>
+								</Link>
+							</div>
+						</div>
+					) : (
+						!loading && (
+							<Link href='/login'>
+								<a className='btn btn-danger px-4 text-white login-header-btn float-right'>
+									Login
+								</a>
+							</Link>
+						)
+					)}
 				</div>
 			</div>
 		</nav>
--- redux/actions/userActions.js
+++ redux/actions/userActions.js
@@ -8,7 +8,7 @@ import {
 	LOAD_USER_FAIL,
 } from '../constants/userConstants';
 
-export const registerUser = (userData) => (dispatch) => {
+export const registerUser = (userData) => async (dispatch) => {
 	try {
 		dispatch({ type: REGISTER_USER_REQUEST });
 		const config = {
@@ -16,7 +16,7 @@ export const registerUser = (userData) => (dispatch) => {
 				'Content-Type': 'Application/json',
 			},
 		};
-		const { data } = axios.post('/api/auth/register', userData, config);
+		const { data } = await axios.post('/api/auth/register', userData, config);
 		dispatch({
 			type: REGISTER_USER_SUCCESS,
 			payload: data,
@@ -26,7 +26,7 @@ export const registerUser = (userData) => (dispatch) => {
 	}
 };
 
-export const loadUser = (userData) => (dispatch) => {
+export const loadUser = (userData) => async (dispatch) => {
 	try {
 		dispatch({ type: LOAD_USER_REQUEST });
 		const config = {
@@ -34,10 +34,11 @@ export const loadUser = (userData) => (dispatch) => {
 				'Content-Type': 'Application/json',
 			},
 		};
-		const { data } = axios.post('/api/me', userData, config);
+		const { data } = await axios.get('/api/me', userData, config);
+		console.log('data ==>', data);
 		dispatch({
 			type: LOAD_USER_SUCCESS,
-			payload: data,
+			payload: data.user,
 		});
 	} catch (error) {
 		dispatch({ type: LOAD_USER_FAIL, error: error.response.data.message });
